7d78ce1c196abf75136ae372192ee420c19ecf3c
Danny Kim <dkim7474@gmail.com>
2016-01-04 17:31:58 -0500

    Added signin/signup forms that do nothing in index.erb and addedsignin route to app.rb

--- app.rb
+++ app.rb
@@ -5,3 +5,17 @@ enable :sessions
 
 set :database, "sqlite3:groupblog.sqlite3"
 
+
+# This route is used for the sign-in and sign-up
+get '/' do 
+	redirect '/signin'
+end
+
+get '/signin' do 
+	erb :index
+end
+
+
+# This will be used for the sign-in and sign-up
+# post '/' do	
+# end
