684ef46ad2f1983d6fadd8940abba0f753761e89
Akeem Spencer <aspencerpsu@gmail.com>
2016-01-04 17:16:55 -0500

    added the post table close #3 close #1 close #2

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160104214240) do
+ActiveRecord::Schema.define(version: 20160104221005) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string  "post"
+    t.integer "user_id"
+    t.string  "title"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
